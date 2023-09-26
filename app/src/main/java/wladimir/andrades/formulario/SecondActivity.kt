31ff13efd34526140080109df2a5c9af7600a6a2
ElWlado <wladimiraaf@gmail.com>
2023-09-26 00:21:27 -0300

    "Integracion BD"

--- app/src/main/java/wladimir/andrades/formulario/SecondActivity.kt
+++ app/src/main/java/wladimir/andrades/formulario/SecondActivity.kt
@@ -1,12 +1,25 @@
 package wladimir.andrades.formulario
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import java.text.SimpleDateFormat
 import java.util.Date
 
 class SecondActivity : AppCompatActivity() {
+    private var db = myDbHelper(this)
+
+    private var name: String = ""
+    private var attribute: String = ""
+    private var rarity: String = ""
+    private var damage: Int = 0
+    private var uses: Int = 0
+    private var date: String = ""
+    private var magic: Boolean = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
@@ -19,21 +32,40 @@ class SecondActivity : AppCompatActivity() {
         val txtDateObtained : TextView = findViewById(R.id.txtDateObtained)
         val txtMagic : TextView = findViewById(R.id.txtMagic)
 
-        val person: Person = intent.getSerializableExtra("Person") as Person
+        val armament: Armament = intent.getSerializableExtra("Person") as Armament
 
-        txtName.text = person.getName()
-        txtAttribute.text = person.getAttribute()
-        txtRarity.text = person.getRarity()
-        txtDamage.text = person.getDamage().toString()
-        txtUses.text = person.getUses().toString()
-        txtDateObtained.text = formatDate(person.getDateObtained())
-        if (person.getMagic()) txtMagic.text = "Sí"
+        name = armament.getName()
+        attribute = armament.getAttribute()
+        rarity = armament.getRarity()
+        damage = armament.getDamage()
+        uses = armament.getUses()
+        date = formatDate(armament.getDateObtained())
+        magic = armament.getMagic()
+
+        txtName.text = name
+        txtAttribute.text = attribute
+        txtRarity.text = rarity
+        txtDamage.text = damage.toString()
+        txtUses.text = uses.toString()
+        txtDateObtained.text = date
+        if (magic) txtMagic.text = "Sí"
         else txtMagic.text = "No"
+    }
+
+    fun saveValues(view: View) {
+        db.addArmament(name, attribute, rarity, damage, uses, date, magic)
+        Toast.makeText(this, "Se guardo el armamento creado.", Toast.LENGTH_SHORT).show()
+        changeDisplay()
     }
 
     private fun formatDate(date: Date): String{
         val dateFormat = SimpleDateFormat("dd/MM/yyyy")
         return dateFormat.format(date)
+    }
+
+    private fun changeDisplay(){
+        intent = Intent(this, ThirdActivity::class.java)
+        startActivity(intent)
     }
 
 }
